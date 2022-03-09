d3fd6ad1f4e16c20abcbd5f98673f1cbfca3075f
wisehead <alex.chenhui@gmail.com>
2022-03-09 11:43:42 +0800

    update

--- 03.Model/HelloWorld/testdb.py
+++ 03.Model/HelloWorld/testdb.py
@@ -12,6 +12,7 @@ def testdb(request):
     return HttpResponse("<p>数据添加成功！</p>")
 """
 
+"""
 # 数据库操作
 def testdb(request):
     # 初始化
@@ -42,3 +43,19 @@ def testdb(request):
         response1 += var.name + " "
     response = response1
     return HttpResponse("<p>" + response + "</p>")
+"""
+
+# 数据库操作
+def testdb(request):
+    # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
+    test1 = Test.objects.get(id=1)
+    test1.name = 'Google'
+    test1.save()
+
+    # 另外一种方式
+    #Test.objects.filter(id=1).update(name='Google')
+
+    # 修改所有的列
+    # Test.objects.all().update(name='Google')
+
+    return HttpResponse("<p>修改成功</p>")
