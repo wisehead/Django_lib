2d9e9cfd8f6cdbb04cf01e1d387ebbe0dcfd00af
wisehead <alex.chenhui@gmail.com>
2022-03-09 09:51:32 +0800

    template ok

---
+++ 02.template/HelloWorld/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def runoob(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'runoob.html', context)
+ 
+def hello(request):
+    return HttpResponse("Hello world ! ")
