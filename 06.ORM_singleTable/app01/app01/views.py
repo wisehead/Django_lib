0ede9c02deaf805b55c79b14f69c87ad95bc189f
wisehead <alex.chenhui@gmail.com>
2022-03-09 15:24:59 +0800

    all

--- 06.ORM_singleTable/app01/app01/views.py
+++ 06.ORM_singleTable/app01/app01/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,HttpResponse
 from app01 import models 
+
+"""
 def add_book(request):
     book = models.Book(title="菜鸟教程",price=300,publish="菜鸟出版社",pub_date="2008-8-8") 
     book.save()
     return HttpResponse("<p>数据添加成功！</p>")
+"""
+
+def add_book(request):
+    books = models.Book.objects.all()
+    print(books,type(books)) # QuerySet类型，类似于list，访问 url 时数据显示在命令行窗口中。
+    return HttpResponse("<p>查找成功！</p>")
