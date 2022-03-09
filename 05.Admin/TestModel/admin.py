2e30d9f3903c3d16561e0d4f41cd166a3fffaebd
wisehead <alex.chenhui@gmail.com>
2022-03-09 14:49:18 +0800

    self defined form

--- 05.Admin/TestModel/admin.py
+++ 05.Admin/TestModel/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from TestModel.models import Test,Contact,Tag
 
+class ContactAdmin(admin.ModelAdmin):
+    fields = ('name', 'email')
+
 # Register your models here.
-admin.site.register([Test, Contact, Tag])
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Test, Tag])
