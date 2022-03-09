b7a65e7934007ccb5c5435d19ef0975798aca056
wisehead <alex.chenhui@gmail.com>
2022-03-09 10:23:47 +0800

    safe

--- 02.template/HelloWorld/views.py
+++ 02.template/HelloWorld/views.py
@@ -26,7 +26,7 @@ def runoob(request):
 """
 
 def runoob(request):
-    views_str = "菜鸟教程"
+    views_str = "<a href='https://www.runoob.com/'>点击跳转</a>"
     return render(request, "runoob.html", {"views_str": views_str})
 
 def hello(request):
