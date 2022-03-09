2e1e0fe62f9113542d513f6bf6e29b5d54c19015
wisehead <alex.chenhui@gmail.com>
2022-03-09 11:44:51 +0800

    delete

--- 03.Model/HelloWorld/testdb.py
+++ 03.Model/HelloWorld/testdb.py
@@ -45,6 +45,7 @@ def testdb(request):
     return HttpResponse("<p>" + response + "</p>")
 """
 
+"""
 # 数据库操作
 def testdb(request):
     # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
@@ -59,3 +60,24 @@ def testdb(request):
     # Test.objects.all().update(name='Google')
 
     return HttpResponse("<p>修改成功</p>")
+"""
+
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+from TestModel.models import Test
+
+# 数据库操作
+def testdb(request):
+    # 删除id=1的数据
+    test1 = Test.objects.get(id=1)
+    test1.delete()
+
+    # 另外一种方式
+    # Test.objects.filter(id=1).delete()
+
+    # 删除所有数据
+    # Test.objects.all().delete()
+
+    return HttpResponse("<p>删除成功</p>")
