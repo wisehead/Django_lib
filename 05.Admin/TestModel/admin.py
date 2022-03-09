6777e651c1b3ec94803baf07129e0be9fc82b86f
wisehead <alex.chenhui@gmail.com>
2022-03-09 14:51:00 +0800

    show/collapse

--- 05.Admin/TestModel/admin.py
+++ 05.Admin/TestModel/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from TestModel.models import Test,Contact,Tag
 
-class ContactAdmin(admin.ModelAdmin):
-    fields = ('name', 'email')
-
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ['Main',{
+            'fields':('name','email'),
+        }],
+        ['Advance',{
+            'classes': ('collapse',), # CSS
+            'fields': ('age',),
+        }]
+    )
+
 admin.site.register(Contact, ContactAdmin)
 admin.site.register([Test, Tag])
