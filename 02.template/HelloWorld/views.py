8548a0c873700c4649b38474f5b34b609a51503e
wisehead <alex.chenhui@gmail.com>
2022-03-09 10:04:24 +0800

    list

--- 02.template/HelloWorld/views.py
+++ 02.template/HelloWorld/views.py
@@ -8,9 +8,15 @@ def runoob(request):
     return render(request, 'runoob.html', context)
 """
 
+"""
 def runoob(request):
   views_name = "菜鸟教程"
   return  render(request,"runoob.html", {"name":views_name})
+"""
+
+def runoob(request):
+    views_list = ["菜鸟教程1","菜鸟教程2","菜鸟教程3"]
+    return render(request, "runoob.html", {"views_list": views_list})
  
 def hello(request):
     return HttpResponse("Hello world ! ")
