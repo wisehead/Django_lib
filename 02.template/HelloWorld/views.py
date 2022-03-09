7ce04e0a2c953da71ebd0469ff59b2b103dc845f
wisehead <alex.chenhui@gmail.com>
2022-03-09 10:25:04 +0800

    if else

--- 02.template/HelloWorld/views.py
+++ 02.template/HelloWorld/views.py
@@ -26,8 +26,8 @@ def runoob(request):
 """
 
 def runoob(request):
-    views_str = "<a href='https://www.runoob.com/'>点击跳转</a>"
-    return render(request, "runoob.html", {"views_str": views_str})
+    views_num = 88
+    return render(request, "runoob.html", {"num": views_num})
 
 def hello(request):
     return HttpResponse("Hello world ! ")
